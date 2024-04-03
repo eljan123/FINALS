7723e77d94af8fcef7e9199d38de9b96f059194e
Eljan <108778203+eljan123@users.noreply.github.com>
2024-04-03 22:22:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> CAFE WHISKERS </title>
+    <link rel="shortcut icon" href="images/logo.png" alt="Logo">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div class="scroll-button">
+    <a href="#home"><i class="fas fa-arrow-up"></i></a>
+</div>
+<nav>
+    <div class="navbar">
+        <img src="images/portfolio-logos-cats-whiskers-cat-cafe.png" class="logopic" alt="Cafe Logo">
+        <ul class="menu">
+            <li><a href="#home">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="">Contacts </a></li>
+            <li><a href="">Menu</Menu></a></li>
+            <li><a href="">Cats</a></li>
+            <li><a href="">Rescue</a></li>
+            <li><a href="">Booking</a></li>
+        </ul>
+    </div>
+    <div class="menu-btn">
+        <i class="fas fa-bars"></i>
+    </div>
+</nav>
+<section class="home" id="home">
+    <div class="home-content">
+    </div>
+</section>
+<section class="about" id="about">
+    <div class="content">
+        <div class="title"><span>About Us</span></div>
+        <div class="about-details">
+            <div class="left">
+                <img src="images/logo1.png" alt="About Cafe">
+            </div>
+            <div class="right">
+                <div class="topic">CAFE WHISKERS</div>
+                <p>(TO BE CONTINUED).</p>
+            </div>
+        </div>
+</section>
+<footer>
+    <div class="text">
+        <span><a href="#"></a>CAFE WHISKERS | Since 2024</span>
+    </div>
+</footer>
+<script src="script.js"></script>
+</body>
+</html>
